7902475aa8b1a2d737d27e82b3a978dc5a3a47f3
Artur S. Farias <arcashaid@gmail.com>
2020-04-17 00:01:15 -0300

    tela de editor

--- pages/control.js
+++ pages/control.js
@@ -1,1 +1,22 @@
 //const file = new custom.File();
+
+function putLines(){
+    let numbers = "";
+    
+    let editor = document.getElementById("editor");
+    let lines = document.getElementById("lines");
+
+    lines.innerHTML = numbers;
+
+    for(let line = 1; line <= editor.scrollHeight / 24;line++){
+        numbers += `${line}<br>`;
+    };
+
+    lines.innerHTML = numbers;
+};
+
+//TODO criar funções para os botões que editam o texto.
+
+
+putLines()
+document.getElementById("editor").oninput = putLines;
--- preload/model.js
+++ preload/model.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // document.getElementById("teste").innerHTML = "testando"
     // document.getElementById('bot').onclick = testes;
   })
-
+//TODO criar as assinaturas dos metodos para manipular arquivos.
 class File{
     constructor(){
     };
