85f325215f15ae551356cb728f2e2eaea96baef0
Artur S. Farias <arcashaid@gmail.com>
2020-04-18 20:24:32 -0300

    mudando visual para photon

--- pages/control.js
+++ pages/control.js
@@ -5,14 +5,17 @@ function putLines(){
     
     let editor = document.getElementById("editor");
     let lines = document.getElementById("lines");
+    let scrollbar = document.getElementsByTagName("body")[0];
 
     lines.innerHTML = numbers;
 
-    for(let line = 1; line <= editor.scrollHeight / 24;line++){
+    for(let line = 1; line <= editor.scrollHeight / 20;line++){
         numbers += `${line}<br>`;
     };
 
     lines.innerHTML = numbers;
+    scrollbar.scrollTop = scrollbar.scrollHeight;
+
 };
 
 //TODO criar funções para os botões que editam o texto.
