de8ad2b81c95a9288cdaa979345e8025af22f9de
Artur S. Farias <arcashaid@gmail.com>
2020-04-24 00:53:38 -0300

    remoçao das linhas, adcionado funcionalidadess

--- pages/buttons.js
+++ pages/buttons.js
@@ -1,1 +1,41 @@
 //TODO criar funções para os botões que editam o texto.
+document.getElementById("bold").onclick = ()=>{
+    selection = window.getSelection()
+    document.execCommand("bold", false, selection);
+};
+
+document.getElementById("italic").onclick = ()=>{
+    selection = window.getSelection()
+    document.execCommand("italic", false, selection);
+};
+
+ document.getElementById("paragraph").onclick = ()=>{
+    document.execCommand('formatBlock', false, '<p>');
+};
+
+document.getElementById("insertHorizontalRule").onclick = ()=>{
+    selection = window.getSelection()
+    document.execCommand("insertHorizontalRule", false, selection);
+};
+
+document.getElementById("justifyLeft").onclick = ()=>{
+    selection = window.getSelection()
+    document.execCommand("justifyLeft", false, selection);
+};
+
+document.getElementById("justifyRight").onclick = ()=>{
+    selection = window.getSelection()
+    document.execCommand("justifyRight", false, selection);
+};
+
+document.getElementById("justifyCenter").onclick = ()=>{
+    selection = window.getSelection()
+    document.execCommand("justifyCenter", false, selection);
+};
+
+document.getElementById("insertOrderedList").onclick = ()=>{
+    selection = window.getSelection()
+    document.execCommand("insertOrderedList", false, selection);
+};
+
+
